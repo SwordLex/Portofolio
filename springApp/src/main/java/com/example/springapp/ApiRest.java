e0aea07d5a14f54119546124fb1f66bcd8c89e97
Alejandro Granell <71333792+SwordLex@users.noreply.github.com>
2025-09-08 14:35:14 +0200

    Add files via upload

---
+++ springApp/src/main/java/com/example/springapp/ApiRest.java
@@ -0,0 +1,11 @@
+package com.example.springapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/persona")
+public class ApiRest {
+
+}
